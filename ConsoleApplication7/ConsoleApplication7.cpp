de22e67de2b8eabf8e309575c9bfa16e543a0258
callmebloom <144678597+callmebloom@users.noreply.github.com>
2024-02-08 01:05:48 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication7/ConsoleApplication7.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+double circle(double radius) {
+    return 3.14 * radius * radius;
+}
+double ring(double outrad, double inrad) {
+    double outarea = circle(outrad);
+    double inarea = circle(inrad);
+    return outarea - inarea;
+}
+int main() {
+    setlocale(LC_ALL, "Rus");
+    double outrad, inrad;
+    std::cout << "введите внешний радиус кольца: ";
+    std::cin >> outrad;
+    std::cout << "введите внутренний радиус кольца: ";
+    std::cin >> inrad;
+    double ringarea = ring(outrad, inrad);
+    std::cout << "площадь кольца получилась " << ringarea << std::endl;
+    return 0;
+}
